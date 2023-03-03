5af7ffd43e6a9f4c42b043b800975a342a0c0862
pavanvermaR0278 <vermapawan242@gmail.com>
2023-03-03 20:57:46 +0530

    commitA

--- master_1.py
+++ master_1.py
@@ -1,6 +1,8 @@
 print("Hi, this is master branch, of service_A repo")
 
 print("This is feature-dev1 brach on top of master branch")
+
+print("this is adding after changes on github branch, to check conflicts")
 n=10
 for i in range(n):
     print(i)
