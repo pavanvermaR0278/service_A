daae8827920d174c4db8be5bfec720b032495658
Pavan verma <vermapawan242@gmail.com>
2023-03-03 20:54:46 +0530

    Update master_1.py
    updating bia github.com

--- master_1.py
+++ master_1.py
@@ -1,6 +1,8 @@
 print("Hi, this is master branch, of service_A repo")
 
 print("This is feature-dev1 brach on top of master branch")
+
+print("I am adding it in github directly")
 n=10
 for i in range(n):
     print(i)
