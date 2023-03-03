d7bead25c91f2233a3f490470eed08edb8e3019d
pavanvermaR0278 <vermapawan242@gmail.com>
2023-03-03 21:12:31 +0530

    fffff

--- master_1.py
+++ master_1.py
@@ -4,6 +4,8 @@ print("This is feature-dev1 brach on top of master branch")
 
 print("this is adding after changes on github branch, to check conflicts")
 n=10
+
+# this is after guthub.com, in vs code editor
 for i in range(n):
     print(i)
 
